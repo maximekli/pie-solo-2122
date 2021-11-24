a6b444849e3cdbad2c9d70bb9561aa33f2c273eb
clementineallanic <clementine.allanic@yahoo.com>
2021-11-24 09:20:19 +0100

    creation of gym environments

---
+++ RL/gym_test.py
@@ -0,0 +1,15 @@
+import gym
+from RL.RL_solo.envs.solo_v1 import SoloEnv
+
+env = SoloEnv
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+env.close()
