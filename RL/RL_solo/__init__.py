7d4e44ed93138d8f3f3d791df6d9d138efecd217
clementineallanic <clementine.allanic@yahoo.com>
2021-11-29 16:12:05 +0100

    main file load own environment

--- RL/RL_solo/__init__.py
+++ RL/RL_solo/__init__.py
@@ -1,4 +1,4 @@
 from gym.envs.registration import register
 
-register(id='hopper-v3',entry_point='RL_solo.envs:HopperEnv',) 
-register(id='solo-v1', entry_point='RL_solo.envs:SoloEnv')
+register(id='hopper-v3',entry_point='envs:HopperEnv',) 
+register(id='solo-v1', entry_point='envs:SoloEnv')
--- RL/RL_solo/envs/__init__.py
+++ RL/RL_solo/envs/__init__.py
@@ -1,2 +1,2 @@
-from RL_solo.envs.hopper_v3 import HopperEnv
-from RL_solo.envs.solo_v1 import SoloEnv
+from .hopper_v3 import HopperEnv
+from .solo_v1 import SoloEnv
