d477dfb6d5a316f65fd78bacf12fe4e2e33cb2e7
Emilie-Thome <emilie.thome@hotmail.fr>
2022-02-15 15:58:41 +0100

    adding new plot resulting CoM function

--- OC/pinocchio_Emilie/inverse_kinematics.py
+++ OC/pinocchio_Emilie/inverse_kinematics.py
@@ -7,68 +7,63 @@ import pinocchio as pin
 import matplotlib.pyplot as plt
 from model_com import *
 
-def computeJointsConfiguration(Rot_ref, CoM_ref, q_init, K=1, q_0=robot.q0, epsilon=1e-4, DT=1e-3):
-    IDX_TOOL    = robot.model.getFrameId('base_link')
-    """
-        ['universe', 'root_joint', 'base_link', 'FL_HAA', 'FL_SHOULDER',
-        'FL_HFE', 'FL_UPPER_LEG', 'FL_KFE', 'FL_LOWER_LEG', 'FL_ANKLE',
-        'FL_FOOT', 'FR_HAA', 'FR_SHOULDER', 'FR_HFE', 'FR_UPPER_LEG',
-        'FR_KFE', 'FR_LOWER_LEG', 'FR_ANKLE', 'FR_FOOT', 'HL_HAA',
-        'HL_SHOULDER', 'HL_HFE', 'HL_UPPER_LEG', 'HL_KFE', 'HL_LOWER_LEG',
-        'HL_ANKLE', 'HL_FOOT', 'HR_HAA', 'HR_SHOULDER', 'HR_HFE', 'HR_UPPER_LEG',
-        'HR_KFE', 'HR_LOWER_LEG', 'HR_ANKLE', 'HR_FOOT']
-    """
-    IT_MAX      = 1000*5
+""" FRAMES SOLO12
+    ['universe', 'root_joint', 'base_link', 'FL_HAA', 'FL_SHOULDER',
+    'FL_HFE', 'FL_UPPER_LEG', 'FL_KFE', 'FL_LOWER_LEG', 'FL_ANKLE',
+    'FL_FOOT', 'FR_HAA', 'FR_SHOULDER', 'FR_HFE', 'FR_UPPER_LEG',
+    'FR_KFE', 'FR_LOWER_LEG', 'FR_ANKLE', 'FR_FOOT', 'HL_HAA',
+    'HL_SHOULDER', 'HL_HFE', 'HL_UPPER_LEG', 'HL_KFE', 'HL_LOWER_LEG',
+    'HL_ANKLE', 'HL_FOOT', 'HR_HAA', 'HR_SHOULDER', 'HR_HFE', 'HR_UPPER_LEG',
+    'HR_KFE', 'HR_LOWER_LEG', 'HR_ANKLE', 'HR_FOOT']
+"""
+frameNames  = ['FR_FOOT', 'FL_FOOT', 'HR_FOOT', 'HL_FOOT']
+FRAME_IDs   = [model.getFrameId(f) for f in frameNames]
+IDX_TOOL    = model.getFrameId('base_link')
+
+
+def computeJacobian(q, IDX_TOOL, FRAME_IDs):
+    # J_CoM       = robot.Jcom(q).copy()
+    J_CoM       = pin.computeFrameJacobian(model, data, q, IDX_TOOL).copy()
+    J_Frames    = np.concatenate([robot.computeFrameJacobian(q, FRAME_ID)[:3].copy() for FRAME_ID in FRAME_IDs], axis=0)
+    return np.concatenate((J_CoM, J_Frames), axis=0)
+
+def computeError(oMgoal, oMtool, placements_ref, placements):
+    # err_CoM     = CoM - CoM_ref
+    err_CoM     = pin.log(oMtool.inverse()*oMgoal).vector
+    err_Frames  = np.concatenate([Fi-Fi_ref for Fi, Fi_ref in zip(placements,placements_ref)], axis=0)
+    return np.concatenate((err_CoM, err_Frames), axis=0)
+
+def computeJointsConfiguration(Rot_ref, CoM_ref, q_init, K=1, q_0=robot.q0, epsilon=1e-3, DT=1e-3):
+    IT_MAX      = 1000
+    ## REFERENCE PLACEMENTS
+    oMgoal              = pin.SE3(Rot_ref,CoM_ref)
+    framePlacements_ref = [robot.framePlacement(q_0, FRAME_ID).translation.copy() for FRAME_ID in FRAME_IDs]
+    ## INITIALIZATION LOOP
     i           = 0
     # q           = q_init.copy()
     # dq          = np.ones(q_0.shape)
-    q           = pin.randomConfiguration(robot.model)
-    dq          = np.random.rand(robot.model.nv)*2-1
-    
-    ## REFERENCE FOOTS PLACEMENTS
-    oMgoal      = pin.SE3(Rot_ref,CoM_ref)
-    F1_ref      = robot.framePlacement(q_0, robot.model.getFrameId('FR_FOOT')).translation.copy()
-    F2_ref      = robot.framePlacement(q_0, robot.model.getFrameId('HR_FOOT')).translation.copy()
-    F3_ref      = robot.framePlacement(q_0, robot.model.getFrameId('FL_FOOT')).translation.copy()
-    F4_ref      = robot.framePlacement(q_0, robot.model.getFrameId('HL_FOOT')).translation.copy()
+    q           = pin.randomConfiguration(model)
+    dq          = np.random.rand(model.nv)*2-1
     while True :
-        # Run the algorithms that outputs values in robot.data
-        pin.framesForwardKinematics(robot.model,robot.data,q)
-        pin.computeJointJacobians(robot.model,robot.data,q)
-        
+        # Run the algorithms that outputs values in data
+        pin.framesForwardKinematics(model,data,q)
+        pin.computeJointJacobians(model,data,q)
         ## CURRENT CONFIGURATION
         # CoM         = robot.com(q).copy()
-        oMtool      = robot.framePlacement(q,IDX_TOOL)
-        F1          = robot.framePlacement(q, robot.model.getFrameId('FR_FOOT')).translation.copy()
-        F2          = robot.framePlacement(q, robot.model.getFrameId('HR_FOOT')).translation.copy()
-        F3          = robot.framePlacement(q, robot.model.getFrameId('FL_FOOT')).translation.copy()
-        F4          = robot.framePlacement(q, robot.model.getFrameId('HL_FOOT')).translation.copy()
-
+        # oMtool          = robot.framePlacement(q,IDX_TOOL)
+        # framePlacements = [robot.framePlacement(q, FRAME_ID).translation.copy() for FRAME_ID in FRAME_IDs]
+        oMtool          = data.oMf[IDX_TOOL].copy()
+        framePlacements = [data.oMf[FRAME_ID].translation.copy() for FRAME_ID in FRAME_IDs]
         ## ERROR
-        # err_CoM     = CoM - CoM_ref
-        err_CoM     = pin.log(oMtool.inverse()*oMgoal).vector
-        err_F1      = F1 - F1_ref
-        err_F2      = F2 - F2_ref
-        err_F3      = F3 - F3_ref
-        err_F4      = F4 - F4_ref
-        err         = np.concatenate((err_CoM, err_F1, err_F2, err_F3, err_F4), axis=0)
-
+        err         = computeError(oMgoal, oMtool, framePlacements_ref, framePlacements)
         ## JACOBIAN FROM CURRENT CONFIGURATION
-        # J_CoM       = robot.Jcom(q).copy()
-        J_CoM       = pin.computeFrameJacobian(robot.model, robot.data, q, IDX_TOOL)
-        J_F1        = robot.computeFrameJacobian(q, robot.model.getFrameId('FR_FOOT'))[:3].copy()
-        J_F2        = robot.computeFrameJacobian(q, robot.model.getFrameId('HR_FOOT'))[:3].copy()
-        J_F3        = robot.computeFrameJacobian(q, robot.model.getFrameId('FL_FOOT'))[:3].copy()
-        J_F4        = robot.computeFrameJacobian(q, robot.model.getFrameId('HL_FOOT'))[:3].copy()
-        # J_Theta     = TODO
-        J           = np.concatenate((J_CoM, J_F1, J_F2, J_F3, J_F4), axis=0)
-
+        J           = computeJacobian(q, IDX_TOOL, FRAME_IDs)
         ## UPDATE CONFIGURATION
-        vq          = pinv(J)@err
-        q           = pin.integrate(robot.model,q, vq * DT)
-        
+        vq          = -K*pinv(J)@err
+        q           = pin.integrate(model,q, vq * DT)
+        ## UPDATE LOOP
         i+=1
-        if not i%100: print(f"norm(err) = {norm(err)}")
+        # if not i%100: print(f"norm(err) = {norm(err)}")
         if norm(err) < epsilon:
             success = True
             break
@@ -79,17 +74,51 @@ def computeJointsConfiguration(Rot_ref, CoM_ref, q_init, K=1, q_0=robot.q0, epsi
         print("Convergence achieved!")
     else:
         print("\nWarning: the iterative algorithm has not reached convergence to the desired precision")
-    return q
+    return q, vq
 
 def computeTrajectory(q_0=robot.q0, T=T):
     Q = []
+    vQ = []
     q = q_0.copy()
+    vq = v_0.copy()
     for t in T:
         if t<=Ts:
-            CoM = 0.5*X_CoM(t)
+            CoM = X_CoM(t)
             Rot = Rot_CoM(t)
             Q.append(("JUMP", q.copy()))
-            q = computeJointsConfiguration(Rot, CoM, q)
+            vQ.append(("JUMP", vq.copy()))
+            q, vq = computeJointsConfiguration(Rot, CoM, q)
         else:
             Q.append(("OHH", q_0.copy()))
-    return Q
+            vQ.append(("OHH", v_0.copy()))
+    return Q, vQ
+
+
+def plot_controlledTrajectory(q_0=robot.q0, T=T, dt=dt):
+    Q, vQ = computeTrajectory(q_0=q_0, T=T)
+    L_X_CoM = []
+    L_V_CoM = []
+    prev_X_CoM = CoM_0.copy()
+    for (_, q), (_, vq), t in zip(*[Q,vQ,T]):
+        if t<=Ts:
+            L_X_CoM.append(robot.com(q).copy())
+            L_V_CoM.append(robot.vcom(q, vq).copy())
+            prev_X_CoM  = L_X_CoM[-1].copy()
+            prev_V_CoM  = L_V_CoM[-1].copy()
+        else:
+            L_X_CoM.append(prev_X_CoM + prev_V_CoM*(t-Ts) + g*(t-Ts)**2/2)
+            L_V_CoM.append(prev_V_CoM + g*(t-Ts))
+            
+    fig1, ax1 = plt.subplots()
+    L_CoM = np.array(L_X_CoM)
+    ax1.plot(T,L_CoM)
+    ax1.plot(T[id_Ts],L_CoM[id_Ts][0],'ro')
+    ax1.plot(T[id_Ts],L_CoM[id_Ts][1],'ro')
+    ax1.plot(T[id_Ts],L_CoM[id_Ts][2],'ro')
+    plt.show(block=False)
+    fig2, ax2 = plt.subplots()
+    ax2.plot(L_CoM[:,0],L_CoM[:,2])
+    plt.show(block=False)
+    fig3, ax3 = plt.subplots()
+    ax3.plot(T,Q)
+    plt.show()
