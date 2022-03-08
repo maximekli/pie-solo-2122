7cb141b9296b959cc933efd1fd5bc4a481b80b23
Mariana-Andrade-Alves <mariana.aa.dias@gmail.com>
2022-03-08 10:04:32 +0100

    cleaned main.py and added plot.py

--- RL/RL_solo/main.py
+++ RL/RL_solo/main.py
@@ -1,23 +1,7 @@
 from training.src.start_training import training as train
 from gym.envs.registration import register
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
 
 if __name__ == '__main__':
-    register(id='hopper-v3', entry_point='envs:HopperEnv')
     register(id='solo-v1', entry_point='envs:SoloEnv')
 
     train()
-
-    data = pd.read_csv('training/training_results/solo-v1/solo-v1_s1/progress.txt',sep='\s+',header=0)
-    data = pd.DataFrame(data)
-    plt.plot(data['Epoch'], data['AverageEpRet'],'r--',label='Average')
-    plt.plot(data['Epoch'], data['MaxEpRet'],'b--',label='Max')
-    plt.plot(data['Epoch'], data['MinEpRet'],'b--',label='Min')
-    plt.xlabel('Epoch')
-    plt.ylabel('EpRet')
-    plt.legend()
-    plt.show()
-    plt.savefig('training/training_results/solo-v1/solo-v1_s1/rewards.png')
